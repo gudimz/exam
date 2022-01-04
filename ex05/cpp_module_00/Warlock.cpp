01b7eb6c557c58636a1f4b529858698eebb38d19
GuDimz <zgudimz@gmail.com>
2022-01-04 10:10:55 +0300

    ex05 start

---
+++ ex05/cpp_module_00/Warlock.cpp
@@ -0,0 +1,24 @@
+#include "Warlock.hpp"
+
+Warlock::Warlock(const std::string& name, const std::string& title) : name(name), title(title) {
+	std::cout << name << ": This looks like another boring day." << std::endl;
+}
+Warlock::~Warlock() {
+	std::cout << name << ": My job here is done!" << std::endl;
+}
+
+void Warlock::introduce() const {
+	std::cout << name << ": I am " << name << ", " << title << "!" << std::endl;
+}
+
+const std::string& Warlock::getName() const {
+	return this->name;
+}
+
+const std::string& Warlock::getTitle() const {
+	return this->title;
+}
+
+void Warlock::setTitle(const std::string& title) {
+	this->title = title;
+}
---
+++ ex05/cpp_module_00/Warlock.hpp
@@ -0,0 +1,22 @@
+#ifndef WARLOCK_HPP
+# define WARLOCK_HPP
+
+#include <iostream>
+
+class Warlock {
+private:
+	std::string name;
+	std::string title;
+	Warlock();
+	Warlock(const Warlock& other);
+	Warlock& operator=(const Warlock& other);
+public:
+	Warlock(const std::string& name, const std::string& title);
+	~Warlock();
+	const std::string& getName(void) const;
+	const std::string& getTitle(void) const;
+	void setTitle(const std::string& title);
+	void introduce(void) const;
+};
+
+#endif
